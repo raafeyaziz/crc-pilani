6875bcffa808ffc9e0e323908a0bf2e3eb14e625
Raafey <raafeyaziz@gmail.com>
2026-09-08 07:41:34 +0530

    fixed backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from finance.views import GoogleLogin,get_csrf_token
 
 from django.conf import settings 
@@ -37,5 +38,10 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
